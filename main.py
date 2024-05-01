44d3bcb2ea013555d559b738ae7f2c88a0650231
Wan Ning <wning825@gmail.com>
2024-05-01 23:53:49 +0800

    added settings

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pygame
-import random
-import os
-import menubutton
+import pygame, random, os, menubutton
+from pygame import mixer
 
 WIDTH = 960
 HEIGHT = 540
@@ -21,38 +19,80 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Peppy the Pizza") # display on top of the window
 clock = pygame.time.Clock() 
 
+#Load and play bg music
+pygame.mixer.music.load("bg_music.mp3")
+#Repeats,when to start playing
+pygame.mixer.music.play(-1,0.0)
+pygame.mixer.music.set_volume(0.3)
+
 all_sprites = pygame.sprite.Group() 
+
 
 #import images from folder
 image_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
 title_path = os.path.join(image_folder, "title.png")
 play_path = os.path.join(image_folder,"play_button.png")
-screen.fill((169, 29, 29))
+option_path=os.path.join(image_folder,"option_button.png")
+exit_path=os.path.join(image_folder,"exit_button.png")
+soundon_path = os.path.join(image_folder, "soundon_button.png")
+soundoff_path = os.path.join(image_folder, "soundoff_button.png")
+back_path= os.path.join(image_folder, "back_button.png")
+#load images
 title_img = pygame.image.load(title_path).convert_alpha()
-title=menubutton.DrawMenu(100,200,title_img,5)
 play_img = pygame.image.load(play_path).convert_alpha()
+option_img = pygame.image.load(option_path).convert_alpha()
+exit_img=pygame.image.load(exit_path).convert_alpha()
+soundon_img = pygame.image.load(soundon_path).convert_alpha()
+soundoff_img = pygame.image.load(soundoff_path).convert_alpha()
+back_img = pygame.image.load(back_path).convert_alpha()
+#create button
+title=menubutton.DrawMenu(100,200,title_img,5)
 play=menubutton.DrawMenu(334,180,play_img,5)
-title.draw(screen)
+option=menubutton.DrawMenu(334,280,option_img,5)
+exit=menubutton.DrawMenu(334,380,exit_img,5)
+sound_button=menubutton.DrawMenu(100,200,soundon_img,5)
+back=menubutton.DrawMenu(500,280,back_img,5)
 
+main_menu=True
+sound_on=True
 # Game loop
 running = True 
 while running: 
     clock.tick(FPS)
-    if play.draw(screen):
-        print('START')
-
-
+    #click X on the right top to close the program
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Update - for sprite to move etc
-    all_sprites.update()
+    #Main Menu
+    if main_menu:
+        screen.fill((169, 29, 29))
+        title.draw(screen)
+        if exit.draw(screen):
+            running=False
+        if play.draw(screen):
+            print("START")
+            # Update - for sprite to move etc
+            all_sprites.update()
+            # Draw / render - draw the sprite onto the screen
+            all_sprites.draw(screen)
+        if option.draw(screen):
+            main_menu=False
+            screen.fill(BLACK)
 
+            
 
-    # Draw / render - draw the sprite onto the screen
-    #screen.fill(BLACK) 
-    all_sprites.draw(screen)
+    else:
+        if back.draw(screen):
+            main_menu=True
+        if sound_button.draw(screen):
+            sound_on=not sound_on
+            if sound_on:
+                sound_button.update_image(soundon_img,5)
+                pygame.mixer.music.play(-1)
+            else:
+                sound_button.update_image(soundoff_img,5)
+                pygame.mixer.music.pause()
 
     pygame.display.flip()
     pygame.display.update() 
--- menubutton.py
+++ menubutton.py
@@ -1,9 +1,11 @@
 import pygame
 
+
 class DrawMenu():
     def __init__(self,x, y, image,scale):
         img_width=image.get_width()
         img_height = image.get_height()
+        self.original_image = image
         self.image=pygame.transform.scale(image,(int(img_width* scale),(img_height*scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft=(x,y)
@@ -23,3 +25,9 @@ class DrawMenu():
             self.clicked= False
         surface.blit(self.image, (self.rect.x, self.rect.y))
         return action
+    
+    def update_image(self, new_image,scale):
+        img_width = self.original_image.get_width()
+        img_height = self.original_image.get_height()
+        self.original_image = new_image  # Update the original image
+        self.image = pygame.transform.scale(new_image, (int(img_width * scale), (img_height * scale)))
