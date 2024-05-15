59b36744e4d57d6002de0892fc37370b46b7d434
Koay <koayyeeshuen@gmail.com>
2024-05-16 00:39:19 +0800

    Add collectibles and enemy movement

--- mainShoot.py
+++ mainShoot.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+import random
 pygame.init()
 
 
@@ -16,6 +16,7 @@ FPS = 60
 
 # define game variables
 GRAVITY = 0.75
+TILE_SIZE = 40
 
 
 # define player action variables
@@ -26,13 +27,29 @@ shoot = False
 # load images
 # Pepperoni (bullet)
 # !!update image
-# pepperoni_img = pygame.image.load('img/pepperoni.png').convert_alpha()
-
+pepperoni_img = pygame.image.load('img/icons/pepperoni.png').convert_alpha()
+#pick up boxes
+health_box_img = pygame.image.load('img/icons/health_box.png').convert_alpha()
+ammo_box_img = pygame.image.load('img/icons/ammo_box.png').convert_alpha()
+item_boxes = {
+	'Health'	: health_box_img,
+	'Ammo'		: ammo_box_img
+}
 
 # define colours
 BG = (252,244,163)
 WOOD_BROWN = (193, 154, 107) 
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+	
+#define font
+font = pygame.font.SysFont('Futura', 30)
 
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 def draw_bg():
     screen.fill(BG)
@@ -60,6 +77,11 @@ class Character(pygame.sprite.Sprite):
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
+        #ai specific variables
+        self.move_counter = 0
+        self.vision = pygame.Rect(0, 0, 150, 20)
+        self.idling = False
+        self.idling_counter = 0
         
         # load all images for the players
         animation_types = ['Idle', 'Roll', 'Jump', 'Dead']
@@ -151,11 +173,44 @@ class Character(pygame.sprite.Sprite):
     def shoot(self):
         if self.shoot_cooldown == 0 and self.ammo > 0:
             self.shoot_cooldown = 20 # reload speed of bullet
-            pepperoni = Pepperoni(self.rect.centerx + (0.6 * self.rect.size[0]*self.direction), self.rect.centery, self.direction)
+            pepperoni = Pepperoni(self.rect.centerx + (0.5 * self.rect.size[0]*self.direction), self.rect.centery, self.direction)
             pepperoni_group.add(pepperoni)
             # reduce ammo
             self.ammo -= 1
+	
+    def ai(self):
+        if self.alive and player.alive:
+            if self.idling == False and random.randint(1, 200) == 1:
+                self.update_action(0)#0: idle
+                self.idling = True
+                self.idling_counter = 50
+			#check if the ai in near the player
+            if self.vision.colliderect(player.rect):
+				#stop running and face the player
+                self.update_action(0)#0: idle
+				#shoot
+                #self.shoot()
+            else:
+                if self.idling == False:
+                    if self.direction == 1:
+                        ai_moving_right = True
+                    else:
+                        ai_moving_right = False
+                    ai_moving_left = not ai_moving_right
+                    self.move(ai_moving_left, ai_moving_right)
+                    self.update_action(1)#1: run
+                    self.move_counter += 1
+                    #update ai vision as the enemy moves
+                    self.vision.center = (self.rect.centerx + 75 * self.direction, self.rect.centery)
 
+                    if self.move_counter > TILE_SIZE:
+                        self.direction *= -1
+                        self.move_counter *= -1
+                else:
+                    self.idling_counter -= 1
+                    if self.idling_counter <= 0:
+                        self.idling = False
+    
     def check_alive(self):
         if self.health <= 0 :
             self.health = 0
@@ -165,6 +220,47 @@ class Character(pygame.sprite.Sprite):
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect) # put character image into the rectangle
+
+class ItemBox(pygame.sprite.Sprite):
+	def __init__(self, item_type, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.item_type = item_type
+		self.image = item_boxes[self.item_type]
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+
+	def update(self):
+		#check if the player has picked up the box
+		if pygame.sprite.collide_rect(self, player):
+			#check what kind of box it was
+			if self.item_type == 'Health':
+				player.health += 25
+				if player.health > player.max_health:
+					player.health = player.max_health
+			elif self.item_type == 'Ammo':
+				player.ammo += 15
+			elif self.item_type == 'Grenade':
+				player.grenades += 3
+			#delete the item box
+			self.kill()
+
+
+class HealthBar():
+	def __init__(self, x, y, health, max_health):
+		self.x = x
+		self.y = y
+		self.health = health
+		self.max_health = max_health
+
+	def draw(self, health):
+		#update with new health
+		self.health = health
+		#calculate health ratio
+		ratio = self.health / self.max_health
+		pygame.draw.rect(screen, BLACK, (self.x - 2, self.y - 2, 154, 24))
+		pygame.draw.rect(screen, RED, (self.x, self.y, 150, 20))
+		pygame.draw.rect(screen, GREEN, (self.x, self.y, 150 * ratio, 20))
 
 class Pepperoni(pygame.sprite.Sprite):
     def __init__(self, x, y, direction):
@@ -191,10 +287,11 @@ class Pepperoni(pygame.sprite.Sprite):
                 player.health -= 5
                 self.kill() # delete bullet 
 
-        if pygame.sprite.spritecollide(enemy, pepperoni_group, False):
-            if enemy.alive:
-                enemy.health -= 25
-                self.kill() # delete bullet 
+        for enemy in enemy_group:
+            if pygame.sprite.spritecollide(enemy, pepperoni_group, False):
+                if enemy.alive:
+                    enemy.health -= 25
+                    self.kill() # delete bullet 
 
 
 
@@ -202,15 +299,25 @@ class Pepperoni(pygame.sprite.Sprite):
 
 
 # create sprite groups
+enemy_group = pygame.sprite.Group()
 pepperoni_group = pygame.sprite.Group()
+item_box_group = pygame.sprite.Group()
 
+#temp - create item boxes
+item_box = ItemBox('Health', 100, 260)
+item_box_group.add(item_box)
+item_box = ItemBox('Ammo', 400, 260)
+item_box_group.add(item_box)
 
 
 
 # create an player instance of the class for player
-player = Character("Peppy",200,200,3, 5, 5)
-enemy = Character("Peppy",400,200,3, 5, 20) # change char type later
-
+player = Character("Peppy",200,200,1.65, 5, 20)
+health_bar = HealthBar(10, 10, player.health, player.health)
+enemy = Character("Pineapple",500, 200, 1.65, 2, 20) # change char type later
+enemy2 = Character('Pineapple', 300, 200, 1.65, 2, 20)		
+enemy_group.add(enemy)		
+enemy_group.add(enemy2)
 
 run = True
 while run:
@@ -218,16 +325,26 @@ while run:
     clock.tick(FPS)
     
     draw_bg()
-    
-    player.update() # change to update to handle all updates together
+    #show player health
+    health_bar.draw(player.health)
+	#show ammo
+    draw_text('AMMO: ', font, WHITE, 10, 35)
+    for x in range(player.ammo):
+        screen.blit(pepperoni_img, (90 + (x * 10), 40))
+          
+    player.update() 
     player.draw()
 
-    enemy.update()
-    enemy.draw()
+    for enemy in enemy_group:
+        enemy.ai()
+        enemy.update()
+        enemy.draw()
 
     # update and draw groups
     pepperoni_group.update()
+    item_box_group.update()
     pepperoni_group.draw(screen)
+    item_box_group.draw(screen)
 
 
     # update player actions
