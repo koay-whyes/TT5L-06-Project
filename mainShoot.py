82fd78deafb69733b22930379f5a64cec50ac444
Chew Jia Yi <agneschew2538@gmail.com>
2024-05-16 11:26:23 +0800

    screen size changed

--- mainShoot.py
+++ mainShoot.py
@@ -4,8 +4,8 @@ import random
 pygame.init()
 
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.5)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Peppy the Pizza')
