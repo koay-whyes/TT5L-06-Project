f7104922d401b156256d294fb60c2fce4282b08d
Wan Ning <wning825@gmail.com>
2024-06-06 12:53:27 +0800

    added stats

--- main.py
+++ main.py
@@ -107,6 +107,27 @@ pizza_stats3_img=pygame.image.load("img/pizza_stats3.png").convert_alpha()
 comic_panel  = pygame.image.load("img/comic_panel.jpeg")
 background_img = pygame.image.load("img/background.jpg")
 
+#Victory
+victory_background_img=pygame.image.load("img/victory/victory_bg.png")
+victory_mainmenu_img=pygame.image.load("img/victory/victory_house.png").convert_alpha()
+victory_next_img=pygame.image.load("img/victory/victory_next.png").convert_alpha()
+victory_settings_img=pygame.image.load("img/victory/victory_settings.png").convert_alpha()
+stats_img=pygame.image.load("img/victory/victory_stats.png").convert_alpha()
+
+#Stats
+#stats_background_img=pygame.image.load("img/stats_background.png")
+stats_return_img=pygame.image.load("img/victory/stats_return.png").convert_alpha()
+stats_add_img=pygame.image.load("img/victory/stats_add_button.png").convert_alpha()
+attack_stats_img=pygame.image.load("img/victory/attack_stats.png").convert_alpha()
+defense_stats_img=pygame.image.load("img/victory/defense_stats.png").convert_alpha()
+health_stats_img=pygame.image.load("img/victory/health_stats.png").convert_alpha()
+pizza_stats0_img=pygame.image.load("img/victory/pizza_stats0.png").convert_alpha()
+pizza_stats1_img=pygame.image.load("img/victory/pizza_stats1.png").convert_alpha()
+pizza_stats2_img=pygame.image.load("img/victory/pizza_stats2.png").convert_alpha()
+pizza_stats3_img=pygame.image.load("img/victory/pizza_stats3.png").convert_alpha()
+
+cheezy_warning_img=pygame.image.load("img/victory/cheezy_warning.png").convert_alpha()
+maximum_level_warning_img=pygame.image.load("img/victory/maximum_level_warning.png").convert_alpha()
 
 #Pause Menu
 pause_img = pygame.image.load("img/pause_button.png").convert_alpha()
@@ -140,7 +161,6 @@ x_button=menubutton.DrawMenu(570,290,x_img,5)
 next_button=menubutton.DrawMenu(850,150,next_img,1.5)
 full_screen_button=menubutton.DrawMenu(850,150,full_scren_img,1.5)
 
-
 #Victory Menu Button
 victory_mainmenu_button=menubutton.DrawMenu(620,350,victory_mainmenu_img,1.5)
 victory_settings_button=menubutton.DrawMenu(725,350,victory_settings_img,1.5)
@@ -148,14 +168,20 @@ victory_next_button= menubutton.DrawMenu(620,120,victory_next_img,3.0)
 stats_button=menubutton.DrawMenu(620,210,stats_img,3.0)
 
 #Stats Menu button
-stats_return_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-stats_add_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-attack_stats_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-defense_stats_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-health_stats_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-pizza_stats1_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-pizza_stats2_button=menubutton.DrawMenu(620,210,stats_img,3.0)
-pizza_stats3_button=menubutton.DrawMenu(620,210,stats_img,3.0)
+stats_return_button=menubutton.DrawMenu(800,380,stats_return_img,2.5)
+stats_add_attack_button=menubutton.DrawMenu(200,250,stats_add_img,2.0)
+stats_add_defense_button=menubutton.DrawMenu(500,50,stats_add_img,2.0)
+stats_add_health_button=menubutton.DrawMenu(800,250,stats_add_img,2.0)
+attack_stats_button=menubutton.DrawMenu(100,230,attack_stats_img,3.0)
+defense_stats_button=menubutton.DrawMenu(400,10,defense_stats_img,4.0)
+health_stats_button=menubutton.DrawMenu(680,200,health_stats_img,4.5)
+cheezy_warning=menubutton.DrawMenu(350,150,cheezy_warning_img,4.5)
+maximum_level_warning=menubutton.DrawMenu(350,150,maximum_level_warning_img,4.5)
+#Stats Menu images
+pizza_stats0=menubutton.DrawMenu(350,150,pizza_stats0_img,10.0)
+pizza_stats1=menubutton.DrawMenu(350,150,pizza_stats1_img,10.0)
+pizza_stats2=menubutton.DrawMenu(350,150,pizza_stats2_img,10.0)
+pizza_stats3=menubutton.DrawMenu(350,150,pizza_stats3_img,10.0)
 
 #reset level
 def reset_level():
@@ -182,14 +208,14 @@ def reset_level():
                 world_data[x][y] = int(tile)
     world = World()
     player, health_bar = world.process_data(world_data)
-    screen_scroll = 0
+    MS.screen_scroll = 0
     MS.player.rect.center = (100,100)
 
 
 def game_over():
     game_over_background_img=pygame.image.load("img/game_over_bg.png")
     game_over_restart_img=pygame.image.load("img/game_over_restart.png").convert_alpha()
-    game_over_restart_button = menubutton.DrawMenu(440,360,game_over_restart_img,2.5)
+    game_over_restart_button = menubutton.DrawMenu(410,360,game_over_restart_img,2.5)
     screen.blit(game_over_background_img,(0,0))
     main_channel.pause()
     game_over_channel.unpause()
@@ -207,7 +233,9 @@ warning=False
 story=False
 victory=False
 stats=False
-
+start_time = pygame.time.get_ticks()
+warning_cheezy_visible = False
+maximum_level_visible=False
 #story
 story_texts = [ 
 
@@ -227,6 +255,9 @@ warning=False
 story=False
 victory=False
 stats=False
+attack_level=0
+defense_level=0
+health_level=0
 # Game loop
 
 running = True 
@@ -295,6 +326,7 @@ while running:
         if back_button.draw(screen):
             main_menu=True
             settings=False
+            stats_channel.pause()
         if sound_button.draw(screen):
             sound_on=not sound_on
             if sound_on:
@@ -325,19 +357,114 @@ while running:
                 main_channel.unpause()
 
     #Victory Menu
-    elif victory:
+    elif victory==True and stats==False:
         main_channel.pause()
         victory_channel.unpause()
         screen.blit(victory_background_img,(0,0))
-        victory_mainmenu_button.draw(screen)
-        victory_settings_button.draw(screen)
-        victory_next_button.draw(screen)
+        if victory_mainmenu_button.draw(screen):
+            main_menu=True
+            victory=False
+            main_channel.unpause()
+            victory_channel.pause()
+        if victory_settings_button.draw(screen):
+            victory=False
+            settings=True
+            main_channel.unpause()
+            victory_channel.pause()
+        if victory_next_button.draw(screen):
+            level_complete=True
+            victory=False
+            victory_channel.pause()
         if stats_button.draw(screen):
             stats=True
             victory=False
         if stats==True:
             stats_channel.unpause()
             screen.fill(WOOD_BROWN)
+    elif stats==True:
+        victory_channel.pause()
+        stats_channel.unpause()
+        screen.fill((255, 224, 130))
+        attack_stats_button.draw(screen)
+        defense_stats_button.draw(screen)
+        health_stats_button.draw(screen)
+        pizza_attack_stats_dict = {1: pizza_stats1, 2: pizza_stats2, 3: pizza_stats3}
+        pizza_defense_stats_dict = {1: pizza_stats1, 2: pizza_stats2, 3: pizza_stats3}
+        pizza_health_stats_dict = {1: pizza_stats1, 2: pizza_stats2, 3: pizza_stats3}      
+        if stats_add_attack_button.draw(screen):
+            if attack_level==3:
+                maximum_level_visible=True
+                start_time = pygame.time.get_ticks()
+            else:
+                if MS.player.cheezy>0:
+                    MS.player.cheezy-=1
+                    attack_level+=1
+                    for enemy in enemy_group:
+                        enemy.health-=1
+                    print(f"attack_level:{attack_level}")
+                    print(f"cheezy:{MS.player.cheezy}")
+                    print(f"enemy health:{enemy.health}")
+                else:
+                    warning_cheezy_visible=True
+                    start_time = pygame.time.get_ticks()
+        if stats_add_defense_button.draw(screen):
+            if defense_level==3:
+                maximum_level_visible=True
+                start_time = pygame.time.get_ticks()
+            else:
+                if MS.player.cheezy>0:
+                    cheezy-=1
+                    defense_level+=1
+                    print(f"defense_level:{defense_level}")
+                    print(f"cheezy:{MS.player.cheezy}")
+                else:
+                    warning_cheezy_visible=True
+                    start_time = pygame.time.get_ticks()
+        if stats_add_health_button.draw(screen):
+            if health_level==3:
+                maximum_level_visible=True
+                start_time = pygame.time.get_ticks()
+            else:
+                if MS.player.cheezy>0:
+                    cheezy-=1
+                    health_level+=1
+                    player.health+=15
+                    print(f"health_level:{health_level}")
+                    print(f"cheezy:{MS.player.cheezy}")
+                    print(f"player health:{player.health}")
+                else:
+                    warning_cheezy_visible=True
+                    start_time = pygame.time.get_ticks()
+  
+        if attack_level in pizza_attack_stats_dict:
+            pizza_attack_stats_dict[attack_level].draw(screen)
+        if defense_level in pizza_defense_stats_dict:
+            pizza_defense_stats_dict[defense_level].draw(screen)
+            if defense_level >= 4:
+                pizza_stats3.draw(screen)
+                print("You have reached the maximum level")
+        if health_level in pizza_health_stats_dict:
+            pizza_health_stats_dict[health_level].draw(screen)
+            if health_level >= 4:
+                pizza_stats3.draw(screen)
+                print("You have reached the maximum level")
+        else:
+            if (attack_level or defense_level or health_level)>= 4:
+                pizza_stats3.draw(screen)
+                print("You have reached the maximum level")
+        if stats_return_button.draw(screen):
+            victory=True
+            stats=False
+            stats_channel.pause()
+        if warning_cheezy_visible:
+            cheezy_warning.draw(screen)
+        if maximum_level_visible:
+            maximum_level_warning.draw(screen)
+        current_time = pygame.time.get_ticks()
+        if current_time - start_time >= 2500:  # 2.5 seconds
+            warning_cheezy_visible = False
+            maximum_level_visible=False
+
     else:
         if not pause_menu:
             # update background
@@ -351,7 +478,7 @@ while running:
             for x in range(player.ammo):
                 screen.blit(pepperoni_img, (125 + (x * 10), 40))
             #show cheezy
-            draw_text(f'x{player.cheezy}', font, WOOD_BROWN, 45, 70)
+            draw_text(f'x{MS.player.cheezy}', font, WOOD_BROWN, 45, 70)
             screen.blit(cheezy_img, (10, 65))
             # for x in range(player.cheezy):
             #   screen.blit(cheezy_img, (100 + (x * 25), 65))
